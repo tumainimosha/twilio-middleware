024205f16daad95847d6cbfb8330647d39133c9d
Tumaini Mosha <princeton.mosha@gmail.com>
2019-01-17 16:48:51 +0300

    Refactored code

--- src/TwilioAuthMiddleware.php
+++ src/TwilioAuthMiddleware.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Princeton255\TwilioMiddleware;
+namespace TwilioMiddleware;
 
 use Closure;
 use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
@@ -11,12 +11,12 @@ class TwilioAuthMiddleware extends AuthenticateWithBasicAuth
 {
     public function handle($request, Closure $next, $guard = null, $field = null)
     {
-        if (app()->environment('production') && ! $this->validateSignature($request)) {
-
+        if (! app()->environment('local') &&
+            ! $this->validateSignature($request)) {
             $msg = 'Invalid signature for request!. Please check if you are sending a valid X-Twilio-Signature header.';
 
             if (config('app.debug')) {
-                $msg .= ' If you are the owner of this API, check if you have set the `services.twilio.secret` config key correctly!';
+                $msg .= ' If you are the owner of this API, check if you have set the `services.twilio.token` config key correctly!';
             }
 
             logger($msg);
@@ -38,7 +38,7 @@ class TwilioAuthMiddleware extends AuthenticateWithBasicAuth
     public function validateSignature($request)
     {
         // Your auth token from twilio.com/user/account
-        $token = config('services.twilio.secret');
+        $token = config('services.twilio.token');
 
         // The X-Twilio-Signature header - in PHP this should be
         // $_SERVER["HTTP_X_TWILIO_SIGNATURE"];
@@ -51,14 +51,18 @@ class TwilioAuthMiddleware extends AuthenticateWithBasicAuth
         // $_SERVER['SCRIPT_URI']
         $url = $request->fullUrl();
 
+        // force SCHEME to https if using SSL proxy.
+        // Fixes issue with generated url being in http when using proxy
+        if ($request->header('X-Forwarded-Proto') === 'https') {
+            $url = str_replace_first('http://', 'https://', $url);
+        }
+
         // The post variables in the Twilio request. You may be able to use
         // $postVars = $_POST
-        $postVars = $request->post();
+        $postVars = $request->isMethod('GET') ? [] : $request->post();
 
-        if ($validator->validate($signature, $url, $postVars)) {
-            return true;
-        } else {
-            return false;
-        }
+        logger("twilio validation details: signature=$signature url=$url postVars=" . print_r($postVars, true));
+
+        return $validator->validate($signature, $url, $postVars);
     }
 }
